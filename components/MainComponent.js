1c2876e9bf0f91287f53a631509c79d704a6aac4
Bobby Keel <bobbykeel76@gmail.com>
2021-07-20 16:49:38 -0400

    HomeComponent ScrollView list homepage

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import Home from './HomeComponent';
 import { createStackNavigator } from 'react-navigation-stack';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View, Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { View, Platform } from 'react-native';
 import Constants from 'expo-constants';
 
 const DirectoryNavigator = createStackNavigator(
@@ -25,36 +27,44 @@ const DirectoryNavigator = createStackNavigator(
   }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home }
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#5637DD'
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: '#fff'
+      }
+    }
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeNavigator },
+    Directory: { screen: DirectoryNavigator }
+  },
+  {
+    drawerBackgroundColor: '#CEC8FF'
+  }
+);
+
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     campsites: CAMPSITES,
-  //     selectedCampsite: null
-  //   };
-  // }
-
-  // onCampsiteSelect(campsiteId) {
-  //   this.setState({ selectedCampsite: campsiteId });
-  // }
   render() {
     return (
       <View 
         style={{
           flex:1,
           paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
-        }}
-      >
+        }}>
         <AppNavigator />
-        {/* <Directory 
-          campsites={this.state.campsites} 
-          onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-        />
-        <CampsiteInfo 
-          campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}
-        /> */}
       </View>
     );
   }
